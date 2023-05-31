c6e306006aabb8ff21b72cb24bf51d78b9b9a508
Gleb Rodukov <electroneuphoria@gmail.com>
2023-05-31 21:56:51 +0300

    g

---
+++ script.js
@@ -0,0 +1,11 @@
+let highlights = document.getElementsByClassName('list-style-none')[18]
+
+highlights.remove()
+let highlight_c = document.createElement("li").classList.add('mt-2');
+let highlight = document.createElement("a").classList.add('Link--primary');
+
+highlights.appendChild(highlight_c);
+highlight_c.appendChild(highlight);
+
+highlight.value = 'gg'
+
