9b70ed2a957a34d218366227a65e1dc261ca5f5d
Gleb Rodukov <electroneuphoria@gmail.com>
2023-06-03 23:39:18 +0300

    Color changer

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 /*
 let highlights = document.getElementsByClassName('list-style-none')[18]
 
-highlights.remove()
-let highlight_c = document.createElement("li").classList.add('mt-2');
-let highlight = document.createElement("a").classList.add('Link--primary');
-
-highlights.appendChild(highlight_c);
-highlight_c.appendChild(highlight);
-
-highlight.value = 'gg'
-*/
-let highlights = document.getElementsByClassName('list-style-none')[18]
-
 let s = document.createElement("div");
 s.classList.add('awesome')
-highlights.appendChild(
-    s
-)
+highlights.appendChild(s)
+*/
+
+const default_colortheme = {
+    "--color-calendar-graph-day-bg": "#161b22",
+    "--color-calendar-graph-day-L1-bg": "#0e4429",
+    "--color-calendar-graph-day-L2-bg": "#006d32",
+    "--color-calendar-graph-day-L3-bg": "#26a641",
+    "--color-calendar-graph-day-L4-bg": "#39d353"
+}
+
+const magenta_colortheme = {
+    "--color-calendar-graph-day-bg": "#161b22",
+    "--color-calendar-graph-day-L1-bg": "#290e44",
+    "--color-calendar-graph-day-L2-bg": "#45006d",
+    "--color-calendar-graph-day-L3-bg": "#6226a6",
+    "--color-calendar-graph-day-L4-bg": "#8b39d3"
+}
+
+const default_colortheme_hacked = {
+    "--color-calendar-graph-day-bg": "#39d353",
+    "--color-calendar-graph-day-L1-bg": "#39d353",
+    "--color-calendar-graph-day-L2-bg": "#39d353",
+    "--color-calendar-graph-day-L3-bg": "#39d353",
+    "--color-calendar-graph-day-L4-bg": "#39d353"
+}
+
+var r = document.querySelector(':root');
+
+
+let styleadmin = (key, value) => {
+  r.style.setProperty(key, value) /* Set new value to variable with key */
+}
+
+for(let theme in magenta_colortheme) {
+    styleadmin(theme, magenta_colortheme[theme])
+}
+
+
---
+++ themes.js
@@ -0,0 +1,11 @@
+const default_colortheme = {
+    "--color-calendar-graph-day-bg": "#161b22",
+    "--color-calendar-graph-day-L1-bg": "#0e4429",
+    "--color-calendar-graph-day-L2-bg": "#006d32",
+    "--color-calendar-graph-day-L3-bg": "#26a641",
+    "--color-calendar-graph-day-L4-bg": "#39d353"
+}
+
+module.exports = {
+    default_colortheme
+}
