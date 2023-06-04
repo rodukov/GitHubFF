3c5dc1379dc605d9eeb5c3edff068198317e603d
Gleb Rodukov <electroneuphoria@gmail.com>
2023-06-04 16:03:54 +0300

    colortheme

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ highlights.appendChild(s)
 */
 
 const default_colortheme = {
-    "--color-calendar-graph-day-bg": "#161b22",
     "--color-calendar-graph-day-L1-bg": "#0e4429",
     "--color-calendar-graph-day-L2-bg": "#006d32",
     "--color-calendar-graph-day-L3-bg": "#26a641",
@@ -15,12 +14,25 @@ const default_colortheme = {
 }
 
 const magenta_colortheme = {
-    "--color-calendar-graph-day-bg": "#161b22",
     "--color-calendar-graph-day-L1-bg": "#290e44",
     "--color-calendar-graph-day-L2-bg": "#45006d",
     "--color-calendar-graph-day-L3-bg": "#6226a6",
     "--color-calendar-graph-day-L4-bg": "#8b39d3"
 }
+
+const haloween_colortheme = {
+    "--color-calendar-graph-day-L1-bg": "#631c03",
+    "--color-calendar-graph-day-L2-bg": "#bd561d",
+    "--color-calendar-graph-day-L3-bg": "#fa7a18",
+    "--color-calendar-graph-day-L4-bg": "#fddf68"
+}
+const winter_colortheme = {
+    "--color-calendar-graph-day-L1-bg": "#0A3069",
+    "--color-calendar-graph-day-L2-bg": "#0969DA",
+    "--color-calendar-graph-day-L3-bg": "#54AEFF",
+    "--color-calendar-graph-day-L4-bg": "#B6E3FF"
+}
+
 
 const default_colortheme_hacked = {
     "--color-calendar-graph-day-bg": "#39d353",
@@ -30,15 +42,24 @@ const default_colortheme_hacked = {
     "--color-calendar-graph-day-L4-bg": "#39d353"
 }
 
-var r = document.querySelector(':root');
 
+
+let selected_colortheme = winter_colortheme
+var r = document.querySelector(':root');
 
 let styleadmin = (key, value) => {
   r.style.setProperty(key, value) /* Set new value to variable with key */
 }
 
-for(let theme in magenta_colortheme) {
-    styleadmin(theme, magenta_colortheme[theme])
+for(let theme in selected_colortheme) {
+    styleadmin(theme, selected_colortheme[theme])
 }
 
+let o = document.getElementsByClassName('Progress-item')
+for(let i = 0; i < o.length; i++) {
+    o[i].style.backgroundColor = selected_colortheme["--color-calendar-graph-day-L4-bg"]
+}
 
+setTimeout(() => {
+    document.getElementsByClassName('js-highlight-blob')[0].setAttribute('stroke', selected_colortheme["--color-calendar-graph-day-L4-bg"])
+}, 1000)
